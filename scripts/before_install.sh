a560e8e20c812734c4eb8e8040b06fa5935b9245
Saurabh Sharma <sausha80@gmail.com>
2022-09-30 03:42:12 +0530

    updated node js version

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/server
 echo 'installing node'
-curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 yum -y install nodejs npm
 echo 'done installing node npm'
