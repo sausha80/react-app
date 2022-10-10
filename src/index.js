f47bfaa1f342a4e6e94e307ea46cb2e912e3bef5
Saurabh Sharma <sausha80@gmail.com>
2022-10-10 21:18:09 +0530

    added one more product airpods

--- src/index.js
+++ src/index.js
@@ -161,7 +161,8 @@ const PRODUCTS = [
     {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
     {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
     {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'airpods'}
 ];
 
 // ========================================
