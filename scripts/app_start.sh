8799ecaec68735b706c0b4353ac07095aab15826
Saurabh Sharma <sausha80@gmail.com>
2022-10-11 02:29:43 +0530

    added logs for shell scripts

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,8 +1,10 @@
 
 #!/bin/bash
+{
 cd /home/ec2-user/server/src
 npm start
 pm2 start npm --name "my-app" -- start
 pm2 startup
 pm2 save
 pm2 restart all
+} > /tmp/app_start_log.log
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+{
 cd /home/ec2-user/server
 echo 'installing node'
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 yum -y install nodejs npm
 echo 'done installing node npm'
+} > /tmp/before_install_log.log
