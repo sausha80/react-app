d18299679b5126fa94d0c50905656b65237932c2
Saurabh Sharma <sausha80@gmail.com>
2022-10-11 02:48:18 +0530

    added fix for npm install failure

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 {
 cd /home/ec2-user/server
+npm config set legacy-peer-deps true
+npm i
 npm install
 npm install --save react react-dom react-scripts react-particles-js
 npm install pm2 -g
